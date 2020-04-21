1342fc3d597ca42e9440ef6ca8bcb86809020fb2
LIHO <tech.support@revenuemonster.my>
2020-04-22 00:23:10 +0800

    chore: refactor code with vue property decorator

--- src/components/Masonry.vue
+++ src/components/Masonry.vue
@@ -1,28 +1,16 @@
 <script lang="ts">
-import Vue from "vue";
+import { Vue, Component, Prop, Emit } from "vue-property-decorator";
 
-export default Vue.extend({
-  props: {
-    cols: {
-      type: [Number],
-      default: 2
-    },
-    gutter: {
-      type: [Number],
-      default: 0
-    },
-    isWaitImage: {
-      type: [Boolean],
-      default: true
-    }
-  },
-  data() {
-    return {
-      masonry: [],
-      loader: [],
-      slotsIndexPointer: 0
-    };
-  },
+@Component
+export default class Masonry extends Vue {
+  @Prop({ type: [Number], default: 2 }) cols!: boolean;
+  @Prop({ type: [Number], default: 0 }) gutter!: boolean;
+  @Prop({ type: [Boolean], default: false }) isWaitImage!: boolean;
+
+  private masonry: Array<any> = [];
+  private loader: Array<any> = [];
+  private slotsIndexPointer: number = 0;
+
   render(createElement) {
     let self = <any>this;
     let columns = <any>[];
@@ -39,6 +27,7 @@ export default Vue.extend({
         })
       );
     }
+
     return createElement(
       "div",
       {
@@ -47,7 +36,8 @@ export default Vue.extend({
       },
       columns
     );
-  },
+  }
+
   created() {
     let self = <any>this;
 
@@ -71,7 +61,8 @@ export default Vue.extend({
       });
       self.slotsIndexPointer = self.$slots.default.length;
     }
-  },
+  }
+
   mounted() {
     let self = <any>this;
     let columnsElem = <any>Array.from(self.$el.children);
@@ -88,22 +79,29 @@ export default Vue.extend({
       });
       this.$emit("isRendered", true);
     }
-  },
+  }
+
   beforeUpdate() {
     let self = <any>this;
     let columnsElem = <any>Array.from(self.$el.children);
-
-    for (let i = self.slotsIndexPointer; i < self.$slots.default.length; i++) {
-      self.masonry.push(
-        <any>new Vue({
-          render(f) {
-            return self.$slots.default[i];
-          }
-        }).$mount()
-      );
+    if (self.$slots.default) {
+      for (
+        let i = self.slotsIndexPointer;
+        i < self.$slots.default.length;
+        i++
+      ) {
+        self.masonry.push(
+          <any>new Vue({
+            render(f) {
+              return self.$slots.default[i];
+            }
+          }).$mount()
+        );
+      }
+      self.slotsIndexPointer = self.$slots.default.length;
     }
-    self.slotsIndexPointer = self.$slots.default.length;
-  },
+  }
+
   updated() {
     let self = <any>this;
     let columnsElem = <any>Array.from(self.$el.children);
@@ -121,51 +119,59 @@ export default Vue.extend({
       });
       this.$emit("isRendered", true);
     }
-  },
-  methods: {
-    isImageLoaded(masonry, callback) {
-      masonry.forEach((children, index) => {
-        children.$el.getElementsByTagName("img").forEach(img => {
-          if (img.complete) {
+  }
+
+  public isImageLoaded(masonry, callback): void {
+    let self = <any>this;
+
+    masonry.forEach((children, index) => {
+      children.$el.getElementsByTagName("img").forEach(img => {
+        if (img.complete) {
+          callback(img.complete, index);
+        } else {
+          img.onload = () => {
             callback(img.complete, index);
-          } else {
-            img.onload = () => {
-              callback(img.complete, index);
-            };
-          }
-        });
-      });
-    },
-    layout() {
-      // Dynamic masonry algorithm
-      let self = <any>this;
-      let columnsElem = <any>Array.from(self.$el.children);
-      let shortestColumn = <any>null;
-      let count = 0;
-
-      self.isImageLoaded(self.masonry, (complete, index) => {
-        if (complete) count++;
-
-        if (self.masonry.length === count) {
-          while (self.masonry.length) {
-            shortestColumn = columnsElem.reduce((prev, cur) => {
-              return prev.clientHeight <= cur.clientHeight ? prev : cur;
-            });
-            shortestColumn.appendChild(self.masonry.shift().$el);
-          }
-
-          if (self.$slots.loader) {
-            columnsElem.forEach((children, i) => {
-              children.appendChild(self.loader[i].$el);
-            });
-          }
-
-          this.$emit("isRendered", true);
+          };
+          img.onerror = () => {
+            // todo: wait for error image to load before render
+            img.src =
+              "https://www.narrowpathwinery.com/assets/images/products/large/no-user-Image.png";
+            callback(img.complete, index);
+          };
         }
       });
-    }
+    });
   }
-});
+
+  public layout(): void {
+    // Dynamic masonry algorithm
+    let self = <any>this;
+    let columnsElem = <any>Array.from(self.$el.children);
+    let shortestColumn = <any>null;
+    let count = 0;
+
+    self.isImageLoaded(self.masonry, (complete, index) => {
+      if (complete) count++;
+
+      if (self.masonry.length === count) {
+        while (self.masonry.length) {
+          shortestColumn = columnsElem.reduce((prev, cur) => {
+            return prev.clientHeight <= cur.clientHeight ? prev : cur;
+          });
+          shortestColumn.appendChild(self.masonry.shift().$el);
+        }
+
+        if (self.$slots.loader) {
+          columnsElem.forEach((children, i) => {
+            children.appendChild(self.loader[i].$el);
+          });
+        }
+
+        this.$emit("isRendered", true);
+      }
+    });
+  }
+}
 </script>
 
 <style lang="scss">
